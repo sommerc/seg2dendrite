2cafb7159576d966e43ba43b961953138ea164de
Christoph Sommer <christoph.sommer@ist.ac.at>
2022-01-28 13:39:17 +0100

    added imaris swc impex

---
+++ imaris_swc_impex/README.md
@@ -0,0 +1,33 @@
+# Imaris XT for Import/Export SWC
+This is Python XTensions for importing/exporting SWC files into/from Imaris
+by Sarun Gulyanon, 28 July 2018, adapted by Christoph Sommer 31 January 2022.
+
+## Installation ##
+  - Install [Python Anaconda](https://www.anaconda.com/download/) (tested on Anaconda 5.2) 
+  - Open "Anaconda prompt" from windows Start menu
+  - Create new python environment by typing and execute command: "conda create -n imaris_XT_py27 python=2.7 numpy -y" (without ")
+  - Activate that environment by: "conda activate imaris_XT_py27" (without ")
+  - Retrieve full path of environment's python executable: "where python" and copy displayed path to clip-board (used later)
+  - Put `importswc.py` and `exportswc.py` in Python XTensions folder, e.g., `C:\Program Files\Bitplane\Imaris x64 9.2.0\XT\python`
+  - Link Python to Imaris by select Files > Preferences.. > CustomTools, and browse the Python 2.7 Application, e.g., `<folder>/Anaconda2/python.exe` <-- (copy from clip-board)
+  - Link the Python XTensions folder by select Files > Preferences.. > CustomTools, and add the XTensions Folders, e.g., `C:\Program Files\Bitplane\Imaris x64 9.2.0\XT\python\`
+
+## How to Use ##
+To import SWC, 
+  1. First, open the image volume.
+  2. Go to Image Processing > Import SWC as Filament
+  3. Select the SWC file using the dialog.
+
+To export SWC,
+  1. First, open the image volume with Filament.
+  2. Second, select the Filament you want to export.
+  3. Go to Image Processing > Export Filament as SWC
+  4. Select the save location using the dialog.
+
+#### Requirements ####
+  - Numpy
+  - ImarisLib (given in Python XTensions folder)
+
+#### Note ####
+  - [Tutorial for making Imaris XT](http://www.scs2.net/next/files/courses/iic/ImarisXTCourse.pdf)
+  - [pIceImarisConnector](http://www.scs2.net/next/index.php?id=110) for testing the code.
---
+++ imaris_swc_impex/importswc.py
@@ -0,0 +1,86 @@
+# Import Filament
+# By Sarun Gulyanon 28 July 2018
+# Adapted by Christoph Sommer 31 Jannuary 2022
+#
+# <CustomTools>
+#  <Menu>
+#    <Item name="Import SWC as Filament" icon="Python">
+#      <Command>PythonXT::ImportSWC(#i)</Command>
+#    </Item>
+#  </Menu>
+# </CustomTools>
+
+import ImarisLib
+import Tkinter
+import tkFileDialog
+import numpy as np
+import time
+import traceback
+
+def ImportSWC(aImarisId):
+    try:
+        # Create an ImarisLib object    
+        vImarisLib = ImarisLib.ImarisLib()
+        # Get an imaris object with id aImarisId
+        vImaris = vImarisLib.GetApplication(aImarisId)
+        # Check if the object is valid
+        if vImaris is None:
+            print 'Could not connect to Imaris!'
+            time.sleep(2)
+            return
+        
+        vFactory = vImaris.GetFactory()
+        vFilaments = vFactory.ToFilaments(vImaris.GetSurpassSelection())
+        
+        # get swc file to load
+        root = Tkinter.Tk()
+        root.withdraw()
+        swcnames = tkFileDialog.askopenfilenames(title = "Select SWC file", filetypes = (("SWC files","*.swc"),("all files","*.*")))
+        root.destroy()
+        
+        for swcname in swcnames:
+            if not swcname: # asksaveasfilename return '' if dialog closed with "cancel".
+                print 'No file was selected.'
+                time.sleep(2)
+                return
+            swc = np.loadtxt(swcname)
+            
+            # get pixel scale in XYZ resolution (pixel/um)
+            V = vImaris.GetDataSet()
+            pixel_scale = np.array([V.GetSizeX() / (V.GetExtendMaxX() - V.GetExtendMinX()), \
+                                    V.GetSizeY() / (V.GetExtendMaxY() - V.GetExtendMinY()), \
+                                    V.GetSizeZ() / (V.GetExtendMaxZ() - V.GetExtendMinZ())])
+            pixel_offset = np.array([V.GetExtendMinX(), V.GetExtendMinY(), V.GetExtendMinZ()])
+            # ad-hoc fix Z-flip when |maxZ| < |minZ|
+            if abs(V.GetExtendMinZ()) > abs(V.GetExtendMaxZ()):
+                pixel_offset = np.array([V.GetExtendMinX(), V.GetExtendMinY(), V.GetExtendMaxZ()])
+                pixel_scale[2] = -pixel_scale[2]
+                
+            # draw Filament
+            N = swc.shape[0]
+            vFilaments = vImaris.GetFactory().CreateFilaments();
+            vPositions = swc[:,2:5].astype(np.float) / pixel_scale
+            vPositions = vPositions + pixel_offset
+            vRadii     = swc[:,5].astype(np.float)
+            vTypes     = swc[:,1] #(0: Dendrite; 1: Spine)
+            vEdges     = swc[:,[6, 0]]
+            idx        = np.all(vEdges > 0, axis=1)
+            vEdges     = vEdges[idx,:] - 1
+            vTimeIndex = 0;
+            vFilaments.AddFilament(vPositions.tolist(), vRadii.tolist(), vTypes.tolist(), vEdges.tolist(), vTimeIndex)
+            vFilamentIndex = 0;
+            vVertexIndex = 1;
+            vFilaments.SetBeginningVertexIndex(vFilamentIndex, vVertexIndex)
+            # Add the filament object to the scene
+            vScene = vImaris.GetSurpassScene();
+            vScene.AddChild(vFilaments, -1)
+            print('Import ' + swcname + ' completed')
+    except:
+        print traceback.format_exc()
+        time.sleep(3)
+    
+        
+        
+        
+    
+    
